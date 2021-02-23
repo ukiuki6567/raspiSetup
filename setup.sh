23a10b5dddcfda89ff92aeac8d02421fb602c26b
Koki Ota <ma20015@shibaura-it.ac.jp>
2021-02-23 14:59:34 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "updating..."
-sudo apt update & sudo apt upgrade -y
-
 echo "setting Raspberry Pi..."
 sudo raspi-config nonint do_change_timezone Asia/Tokyo
 sudo raspi-config nonint do_change_locale ja_JP.UTF-8
 sudo raspi-config nonint do_i2c 0
 sudo raspi-config nonint do_spi 0
 
+echo "updating..."
+sudo apt update -yqq & sudo apt upgrade -yqq
+
 echo "install tools..."
-sudo apt install -y git emacs pigpio
+sudo apt install -y emacs pigpio
 
 echo "install nodejs&nodered"
 sudo apt install -y nodejs npm
