14792028c2544d7086b1f5e70425f3afcdeb574e
Koki Ota <ma20015@shibaura-it.ac.jp>
2021-11-01 17:41:43 +0900

    repackSetup udpated

--- repackSetup.sh
+++ repackSetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "updating..."
-sudo apt update -y & sudo apt upgrade -y
+sudo apt update -y --allow-releaseinfo-change & sudo apt upgrade -y
 
 sudo cp ./wpa_supplicant.conf /etc/wpa_supplicant/
 sudo raspi-config nonint do_hostname raspberrypi$1
